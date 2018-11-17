f0354a4ee0cf63711eaa0cb657f4eaced8589f2e
Austin Stephens <austin_stephens57@yahoo.com>
2018-11-17 01:08:30 -0500

    Added Logging
    Uses spdlog

--- QuantumEngine/src/Quantum.h
+++ QuantumEngine/src/Quantum.h
@@ -3,6 +3,7 @@
 // Only to be used by Quantum Applications
 
 #include "Quantum\Application.h"
+#include "Quantum\Log.h"
 
 // Entry Point
 //----------------------------------------------------
--- QuantumEngine/src/Quantum/EntryPoint.h
+++ QuantumEngine/src/Quantum/EntryPoint.h
@@ -5,6 +5,10 @@
 extern Quantum::Application* Quantum::CreateApplication();
 
 int main(int argc, char** argv) {
+	Quantum::Log::Init();
+	QT_CORE_WARN("init");
+	int a = 5;
+	QT_INFO("Init Var={0}", a);
 	auto app = Quantum::CreateApplication();
 	app->Run();
 	delete app;
